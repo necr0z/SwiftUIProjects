727034855065b7c7a9f650b8a16c8bc36c086956
necr0z <necroz1998@gmail.com>
2024-07-05 18:24:54 +0800

    CheckoutView line55 breakpoint error

--- CupcakeCorner/CupcakeCorner/AddressView.swift
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -2,37 +2,33 @@
 //  AddressView.swift
 //  CupcakeCorner
 //
-//  Created by Glen Chen on 4/7/24.
+//  Created by Paul Hudson on 10/11/2023.
 //
 
 import SwiftUI
 
 struct AddressView: View {
-    
-    
     @Bindable var order: Order
+
     var body: some View {
-        NavigationStack{
+        Form {
             Section {
                 TextField("Name", text: $order.name)
-                TextField("Address", text: $order.streetAddress)
+                TextField("Street Address", text: $order.streetAddress)
                 TextField("City", text: $order.city)
-                TextField("Zipcode", text: $order.zip
-                )
+                TextField("Zip", text: $order.zip)
             }
+
             Section {
-                NavigationLink("Checkout"){
+                NavigationLink("Check out") {
                     CheckoutView(order: order)
                 }
             }
             .disabled(order.hasValidAddress == false)
-            .navigationTitle("Delivery Address")
-            .navigationBarTitleDisplayMode(.inline)
-
         }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
     }
-    
-    
 }
 
 #Preview {
--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -1,42 +1,49 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Paul Hudson on 09/11/2023.
+//
 
 import SwiftUI
-import CoreHaptics
-
-
 
 struct ContentView: View {
     @State private var order = Order()
-    
+
     var body: some View {
-        NavigationStack{
-            Form{
-                Section{
-                    Picker("Select cake type", selection: $order.type){
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
                         ForEach(Order.types.indices, id: \.self) {
                             Text(Order.types[$0])
                         }
                     }
-                    Stepper("number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
-                    
+
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
                 }
-                Section{
-                    Toggle("Any special things?", isOn: $order.specialRequestEnabled)
-                    
+
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
+
                     if order.specialRequestEnabled {
-                        Toggle("extra frosting", isOn:$order.extraFrosting)
-                        Toggle("add sprinkles", isOn:$order.addSprinkles)
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
                     }
                 }
-                Section{
-                    NavigationLink("deliery details"){
-                        AddressView(order: Order())
+
+                Section {
+                    NavigationLink("Delivery details") {
+                        AddressView(order: order)
                     }
                 }
             }
-            
-            
-            
             .navigationTitle("Cupcake Corner")
         }
     }
+}
+
+#Preview {
+    ContentView()
 }
